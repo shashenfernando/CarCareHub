f4da2ac23540922bfacd0c574dd106402c2757f1
ShashenF <shashenf71@gmail.com>
2024-07-10 21:59:04 +0530

    Initial commit

---
+++ src/main/java/com/example/carcarehub/CarCareHubApplication.java
@@ -0,0 +1,14 @@
+package com.example.carcarehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCareHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCareHubApplication.class, args);
+        System.out.println("Car Care Hub");
+    }
+
+}
---
+++ src/test/java/com/example/carcarehub/CarCareHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carcarehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCareHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
