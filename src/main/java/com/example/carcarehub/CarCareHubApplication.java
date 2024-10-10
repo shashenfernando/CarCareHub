131284c4a771d8e1a9ed1bd24d3f041becbc5ff3
ShashenF <shashenf71@gmail.com>
2024-10-10 21:04:12 +0530

    Added

--- src/main/java/com/example/carcarehub/CarCareHubApplication.java
+++ src/main/java/com/example/carcarehub/CarCareHubApplication.java
@@ -9,10 +9,6 @@ public class CarCareHubApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarCareHubApplication.class, args);
         System.out.println("Car Care Hub");
-        System.out.println("Shashen");
-        System.out.println("Nimash");
-        System.out.println("Malindu");
-        System.out.println("Nipun");
     }
 
 }
---
+++ src/main/java/com/example/carcarehub/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.carcarehub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET , value = "/CarCareHub")
+    public String home(){
+        return "Home";
+    }
+
+}
