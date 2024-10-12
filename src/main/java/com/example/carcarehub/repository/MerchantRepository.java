60bbf3efac8150d146bd6b0d088c106465a010e0
ShashenF <shashenf71@gmail.com>
2024-10-12 21:34:41 +0530

    Update

--- src/main/java/com/example/carcarehub/repository/MerchantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.carcarehub.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MerchantRepository {
-
-}
--- src/main/java/com/example/carcarehub/service/AdminService.java
+++
@@ -1,4 +0,0 @@
-package com.example.carcarehub.service;
-
-public interface AdminService {
-}
