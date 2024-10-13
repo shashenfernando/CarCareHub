f8394d3f65dfa5c07ab755e8a6febd614b0eaeb8
Nipunpv <bhagyanipun01@gmail.com>
2024-10-13 13:08:57 +0530

    changed author and added class

---
+++ src/main/java/com/example/carcarehub/domain/DayOfWeek.java
@@ -0,0 +1,34 @@
+package com.example.carcarehub.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class DayOfWeek {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String day;
+
+    private String openTime;
+    private String closeTime;
+    @OneToOne(mappedBy="dayOfWeek")
+    private Count count;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+}
